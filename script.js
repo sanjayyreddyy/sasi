75e3a3839b48917e4ec6e9186891f871fcfc78bd
Sanjay Reddy <50801760+sanjayyreddyy@users.noreply.github.com>
2020-08-14 22:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var cycle = 0; 
 var allBackgrounds = ["#FFFFFF","#FF0000 ","#00FF00","#0000FF","#FFFF00","#00ff00","#ff0080","#66ffff","#66ff66","#FFFFFF","#FF0000 ","#00FF00","#0000FF","#FFFF00","#00ff00","#ff0080","#66ffff","#66ff66"]; 
 var a = ["34","34","34","34","34","34","34","34","34","abba sasi","abba sasi","abba sasi","abba sasi","em unnado"]; 
-var siz=["40px","50px","60px","80px","100px","160px","250px","300px","300px","60px","80px","100px","120px","150px","200px","200px"];
+
+var siz=["40px","50px","60px","80px","100px","160px","250px","200px","200px","60px","80px","100px","120px","150px","200px","200px"];
 
 setInterval(function() { 
 	if (cycle < 14) { 
